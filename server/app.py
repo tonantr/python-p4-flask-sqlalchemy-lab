0c4e7a2f3fd5b3d0946582c01ff547da7429dc2b
tonan <126637328+tonantr@users.noreply.github.com>
2023-09-10 23:45:52 -0400

    Flask-SQLAlchemy Lab

--- server/app.py
+++ server/app.py
@@ -6,29 +6,105 @@ from flask_migrate import Migrate
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+NOT_FOUND_ERROR_MESSAGE = "<h1>404 Not Found</h1>"
+
+
+@app.route("/")
 def home():
-    return '<h1>Zoo app</h1>'
+    return "<h1>Zoo app</h1>"
 
-@app.route('/animal/<int:id>')
+
+@app.route("/animal/<int:id>")
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
 
-@app.route('/zookeeper/<int:id>')
+    if not animal:
+        response_body = f"{NOT_FOUND_ERROR_MESSAGE}"
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f"""
+    <pre>
+    <ul style="list-style-type:none">
+        <li>ID: {animal.id}</li> 
+    </ul>
+
+    <ul style="list-style-type:none">
+        <li>Name: {animal.name}</li>
+    </ul>
+
+    <ul style="list-style-type:none">
+        <li>Species: {animal.species}</li>
+    </ul>
+
+    <ul style="list-style-type:none">
+        <li>Zookeeper: {animal.zookeeper.name}</li>
+    </ul>
+
+    <ul style="list-style-type:none">
+        <li>Enclosure: {animal.enclosure.environment}</li>
+    </ul>
+    </pre>
+    """
+    response = make_response(response_body, 200)
+    return response
+
+
+@app.route("/zookeeper/<int:id>")
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
 
-@app.route('/enclosure/<int:id>')
+    if not zookeeper:
+        response_body = f"{NOT_FOUND_ERROR_MESSAGE}"
+        response = make_response(response_body, 404)
+        return response
+
+    animal_list = "\n".join([f"Animal: {animal.name}" for animal in zookeeper.animals])
+    response_body = f"""
+    <pre>
+    <ul style="list-style-type:none">
+        <li>ID: {zookeeper.id}</li>
+        <li>Name: {zookeeper.name}</li>
+        <li>Birthday: {zookeeper.birthday}</li>
+        <li>{animal_list}</li>
+    </ul>
+    </pre>
+    """
+    response = make_response(response_body)
+    return response
+
+
+@app.route("/enclosure/<int:id>")
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+
+    if not enclosure:
+        response_body = f'{NOT_FOUND_ERROR_MESSAGE}'
+        response = make_response(response_body)
+        return response
+    
+    animal_list = '\n'.join([f'Animal: {animal.name}' for animal in enclosure.animals])
+    response_body = f"""
+    <pre>
+    <ul style="list-style-type:none">
+        <li>ID: {enclosure.id}</li>
+        <li>Environment: {enclosure.environment}</li>
+        <li>Open to Visitors: {enclosure.open_to_visitors}</li>
+        <li>{animal_list}</li>
+    </ul>
+    </pre>
+    """
+
+    response = make_response(response_body)
+    return response
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
